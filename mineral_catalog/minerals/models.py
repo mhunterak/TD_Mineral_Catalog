d3741ac0ac06752e61e756e7205a4caed5092a83
Maxwell Hunter <maxwell@treehousestickers.com>
2018-12-30 15:04:31 -0800

    Update
    -added img assets
    -updated random display
    -updated details page

--- mineral_catalog/minerals/models.py
+++ mineral_catalog/minerals/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 
+def key_w_spaces(key):
+    display_list = list(key)
+    display_list[0] = display_list[0].upper()
+    for i, char in enumerate(display_list):
+        if char == '_':
+            display_list[i] = ' '
+            display_list[i+1] = display_list[i+1].upper()
+    return "".join(display_list)+':'
+
+
 # Create your models here.
 class Mineral(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        )
     name = models.CharField(max_length=255)
     image_filename = models.CharField(max_length=255)
     image_caption = models.CharField(max_length=255)
@@ -25,5 +38,21 @@ class Mineral(models.Model):
     crystal_habit = models.CharField(max_length=255)
     specific_gravity = models.CharField(max_length=255)
 
+    def keys(self):
+        return (self.__dict__.keys())
+
+    def values(self):
+        return (self.__dict__.values())
+
+    def kv_list(self):
+        kv_list = []
+        for key in self.__dict__.keys():
+            if key not in ['_state', 'created_at', 'id', 'image_filename', 'image_caption']:
+                if self.__dict__[key] is not '':
+                    display_key = key_w_spaces(key)
+                    sub_list = [display_key, self.__dict__[key]]
+                    kv_list.append(sub_list)
+        return kv_list
+
     def __str__(self):
-        return self.name
+        return str(self.__dict__)
--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,15 +1,19 @@
 import json
 import os
+import random
+import re
 
 from django.core.files import File
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from minerals.models import Mineral
 from mineral_catalog.settings import STATICFILES_DIRS
 
 
 # Create your views here.
+
+
 def mineral_list(request):
     minerals = Mineral.objects.all()
     return render(
@@ -24,21 +28,40 @@ def mineral_detail(request, pk):
     return render(
         request,
         'detail.html',
-        {'mineral': mineral}
+        {
+            'mineral': mineral,
+            'kv_list': mineral.kv_list(),
+            'keys': mineral.keys(),
+            'values': mineral.values(),
+        }
     )
 
+def random_mineral(request):
+    random_id = random.randint(0, Mineral.objects.count())
+    return redirect('/{}_detail/'.format(random_id))
 
 def load_new_minerals(request):
     with open('./assets/minerals.json', 'r') as f:
-        print('================')
-        print(str(f))
-        print('================')
-        j = json.loads(str(f))
+        json_string = ''
+        for line in f:
+            json_string += str(line)
+        json_list = json.loads(json_string)
+        for dict in json_list:
+            mineral = Mineral(
+                name=dict['name']
+            )
+            mineral.__dict__.update(dict)
+            for key in dict.keys():
+                if " " in key:
+                    underscore_key = "_".join(key.split(" "))
+                    setattr(mineral, underscore_key, dict[key])
+            mineral.save()
+        return HttpResponse(Mineral.objects.all())
 
-        pass
 
-
-def home(request):
+def index(request):
+    minerals = Mineral.objects.all()
     return render(
         request,
-        'index.html')
+        'index.html',
+        {'minerals': minerals})
