450352369db1c29950bd6a5cd110db28d7684c0a
Maxwell Hunter <maxwell@treehousestickers.com>
2019-01-02 12:11:14 -0800

    added flash messages
    and finished tests, project should be set for delivery

--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from minerals.models import Mineral
@@ -21,7 +22,14 @@ def mineral_list(request):
 
 def mineral_detail(request, pk):
     '''This function is the view for showing a specific mineral detail view'''
-    mineral = Mineral.objects.get(pk=pk)
+    try:
+        mineral = Mineral.objects.get(pk=pk)
+    except Mineral.DoesNotExist:
+        messages.add_message(
+            request,
+            messages.INFO,
+            "We couldn't find a mineral for that ID. Please pick a new one: ")
+        return redirect('/')
     return render(
         request,
         'detail.html',
@@ -33,5 +41,11 @@ def random_mineral(request):
     '''This function is the view for selecting a random mineral'''
     # generate random number between 1 and the number of minerals we have
     random_id = random.randint(1, Mineral.objects.count())
+    # show a message to show that a random mineral was generated
+    messages.add_message(
+        request,
+        messages.INFO,
+        "Random Mineral #{}:".format(random_id))
+
     # return a redirect the detail page for that mineral
-    return redirect('/{}_detail/'.format(random_id))
+    return redirect('/detail/{}'.format(random_id))
